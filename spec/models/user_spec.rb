219437c0a4b3b49605d6352afaca814fd4658baf
Michael Levens <micahel@physprac.com>
2013-10-11 16:21:07 +1100

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "fooba", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -108,7 +108,15 @@ describe "with a password that's too short" do
   it { should be_invalid }
 end
 
+ describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 
 
 end
